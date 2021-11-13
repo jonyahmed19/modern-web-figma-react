7fa763aa76eef955efbe9df6931fb2f8fa8a479e
Sahidul Islam <jonyahmed19@gmail.com>
2021-11-13 19:59:41 +0600

    Completed file setup and designed the header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./App.css";
+
 import { Article, Brand, Cta, Feature, Navbar } from "./components/";
 import {
   Blog,
@@ -11,8 +13,18 @@ import {
 
 const App = () => {
   return (
-    <div>
-      <Article />
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 };
